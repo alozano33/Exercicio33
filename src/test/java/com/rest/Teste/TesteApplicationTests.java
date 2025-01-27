952a89dac8091d5d0d86d6fbcc8db00da083e377
Alessandro Lozano <alessandrolozano@MacBook-Pro-de-Alessandro.local>
2025-01-27 16:52:00 -0300

    Initial commit

---
+++ src/test/java/com/rest/Teste/TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.Teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
